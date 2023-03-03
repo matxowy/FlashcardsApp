667fce024b0c8bc0704a35ef3ae623c13a9a0f45
matxowy <suskamateusz11@gmail.com>
2023-03-03 09:23:02 +0100

    [FCA-04]
    - Implemented main screen
    - Added colors and themes from theme builder to support dark mode
    - Added basic dimensions in dimens.xml
    - Disabled jetifier to improve building time
    - Downgraded material version to 1.7 due to problem with rendering with TextInputLayout

---
+++ app/src/main/java/com/matxowy/flashcardsapp/presentation/mainscreen/viewmodel/MainScreenViewModel.kt
@@ -0,0 +1,8 @@
+package com.matxowy.flashcardsapp.presentation.mainscreen.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainScreenViewModel @Inject constructor() : ViewModel()
