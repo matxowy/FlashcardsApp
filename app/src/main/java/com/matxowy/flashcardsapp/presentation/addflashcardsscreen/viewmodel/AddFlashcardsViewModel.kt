71c61ba9688f81e73ceae0ecfefbadf9c2803f9c
matxowy <suskamateusz11@gmail.com>
2023-03-07 11:50:42 +0100

    [FCA-08]
    - Added add flashcard screen
    - Added navigation to flashcard screen
    - Some code refactor

---
+++ app/src/main/java/com/matxowy/flashcardsapp/presentation/addflashcardsscreen/viewmodel/AddFlashcardsViewModel.kt
@@ -0,0 +1,10 @@
+package com.matxowy.flashcardsapp.presentation.addflashcardsscreen.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AddFlashcardsViewModel @Inject constructor() : ViewModel() {
+    // TODO: Implement the ViewModel
+}
